0272af2cf68293ba3318f331ee5f4a484a431732
fakailo <68045774+fakailo@users.noreply.github.com>
2020-07-09 17:44:28 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    led.unplot(posX, 0)
+    posX += 1
+    led.plot(posX, 0)
+})
+let posX = 0
+posX = 0
+led.plot(posX, 0)
+basic.forever(function () {
+	
+})
